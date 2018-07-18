716f7ef25c940279f044e5087ded42b1bc8f4f8e
Wojciech Misztal <wojciech.misztal@poleng.pl>
2018-07-18 13:20:43 +0200

    MutationObserver added

--- js/gameManager.js
+++ js/gameManager.js
@@ -1,24 +1,46 @@
 class GameManager {
     static set(className, gameAreaId, withoutDropdown = false, withoutSnackbar = false) {
-        $("#" + gameAreaId).empty();
-        Locale.setLanguage();
-        GameManager.game = new className(gameAreaId);
-        if (!withoutDropdown)
-            Dropdown.setDropdown();
-        if (!withoutSnackbar) {
-            Snackbar.setSnackbar();
-            Snackbar.show("info", '_start_header', '_start');
+        var setUp = function () {
+            $("#" + gameAreaId).empty();
+            Locale.setLanguage();
+            GameManager.game = new className(gameAreaId);
+            if (!withoutDropdown)
+                Dropdown.setDropdown();
+            if (!withoutSnackbar) {
+                Snackbar.setSnackbar();
+                Snackbar.show("info", '_start_header', '_start');
+            }
         }
-        // For moodle
+
         var resourceObject = window.top.document.getElementById('resourceobject');
         if(resourceObject) {
+            if(!resourceObject.style.height){
+                var observer = new MutationObserver(function(e) {
+                    if(e[0].target.style.height){
+                        setUp();
+                        this.disconnect();
+                    }
+                });
+                observer.observe(resourceObject, {
+                    attributes: true,
+                    childList: false,
+                    characterData: true,
+                    subtree: false
+                });
+            }
+            else {
+                setUp();
+            }
+
+            // For moodle
             resourceObject.style.border = '0px';
         }
-        
+        else {
+            setUp();
+        }
     }
 
     static get() {
         return GameManager.game;
     }
-
 }
