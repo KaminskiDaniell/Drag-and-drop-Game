a058950ef903231b318e31282cbc06f3de7a8dda
Wojciech Misztal <wojciech.misztal@poleng.pl>
2018-07-24 13:00:13 +0200

    Crossword fixes

--- js/CrosswordGame/crosswordGame.js
+++ js/CrosswordGame/crosswordGame.js
@@ -6,7 +6,7 @@ class CrosswordGame extends Game {
     }
 
     loadGame() {
-        this.crossword= $('<div>', {id: 'crossword'});
+        this.crossword= $('<table>', {id: 'crossword'});
         this.crosswordType = CrosswordGame.crosswordType;
         this.getGameArea().append(this.crossword);
 
@@ -18,6 +18,8 @@ class CrosswordGame extends Game {
             var word = this.getCrosswordData()[i];
             this.gameObjects.push(new WordGameObject(word, i, lowestSolutionLetter, this))
         }
+
+        this.setFocus(0);
     }
 
     getCrosswordData() {
@@ -38,6 +40,39 @@ class CrosswordGame extends Game {
         }
 
         return this.lowestSolutionLetter;
+    }
+
+    insertLetter(letter) {
+        console.log(letter);
+        if(letter === "Backspace" || letter === "ArrowLeft") {
+            this.gameObjects[this.focus].prevLetter();
+        }
+        if(letter === 'Tab' || letter === "ArrowRight") {
+
+        }
+        else if(this.gameObjects[this.focus].insertLetter(letter) === 'next') {
+            this.setFocus(this.focus + 1);
+        }
+    }
+
+    setFocus(wordNumber) {
+        if(typeof wordNumber == 'undefined') {
+            wordNumber = this.focus ? this.focus : 0;
+        }
+        if(typeof this.focus == 'undefined') {
+            this.focus = wordNumber;
+        }
+        else {
+            this.gameObjects[this.focus].unsetActive();
+            if(wordNumber > this.gameObjects.length - 1) {
+                this.focus = 0;
+            }
+            else {
+                this.focus = wordNumber;
+            }
+        }
+        this.gameObjects[this.focus].setActive();
+        this.gameObjects[this.focus].setFocus();
     }
 
     static loadImages() {
