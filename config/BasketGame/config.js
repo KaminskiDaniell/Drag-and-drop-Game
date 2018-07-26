3cf7cabafbfdb3c0ed85172be126f805dcf9d5ca
Artur Wozniak <artur.wozniak@poleng.pl>
2018-07-26 10:07:19 +0200

    readme for basket, quiz, sort and puzzle added

--- config/BasketGame/config.js
+++ config/BasketGame/config.js
@@ -1,90 +1,38 @@
 BasketGame.versions = {
-    'alpha': {
-        basketHeight: 75, //percentage
-        buttonHeight: 50, //percentage
+    "alpha": {  //version of the game
+        "basketHeight": 75, //percentage
+        "buttonHeight": 50, //percentage
 
-        horizontalOffset: 150,
-        verticalOffset: 140,
-        sources: {
-            '_worth': [
-                {
-                    'display': '_citizen_kane',
-                    'name': 'GOOD',
-                },
-                {
-                    'display': '_vertigo',
-                    'name': 'GOOD',
-                }
-            ],
-            '_not_worth': [
-                {
-                    'display': '_la_regle_du_jeu',
-                    'name': 'GOOD',
-                },
-                {
-                    'display': '_2001_a_space_odyssey',
-                    'name': 'GOOD',
-                },
-                {
-                    'display': '_tokyo_monogatari',
-                    'name': 'GOOD'
-                },
-                {
-                    'fileName': "6.jpg",
-                    'display': "_otto_e_mezzo",
-                    'name': 'GOOD',
-                },
-                {
-                    'fileName': "7.jpg",
-                    'display': "_the_godfather",
-                    'name': 'GOOD'
-                }
-            ],
-        }
-    },
-    'beta': {
-        basketHeight: 75, //percentage
-        buttonHeight: 50, //percentage
-
-        horizontalOffset: 150,
-        verticalOffset: 140,
-        sources: {
-            '_yes':
+        "horizontalOffset": 150, //for shuffling on screen
+        "verticalOffset": 140, //for shuffling on screen
+        "sources": {
+            "_yes":  //first basket name (to localize)
                 [
                     {
-                        'display': '_put',
-                        'name': 'GOOD',
+                        "display": "_put", //first element name (to localize)
+                        "name": "GOOD"  // GOOD = put in this basket,
+                        //BAD is used only to add elements with no matching basket
                     },
                     {
-                        'display': '_put1',
-                        'name': 'GOOD',
+                        "fileName": "1.jpg", //if fileName given, element will be image with src 1.jpg (in /img/ folder)
+                        "display": "_put1",
+                        "name": "BAD" //dont put into basket
                     }
                 ],
-            '_no':
+            "_no": //second basket name (to localize)
                 [
                     {
-                        'display': '_dont',
-                        'name': 'GOOD',
-                    },
-                    {
-                        'display': '_dont1',
-                        'name': 'GOOD',
-                    },
-                    {
-                        'display': '_dont2',
-                        'name': 'GOOD'
-                    },
+                        "display": "_dont",
+                        "name": "GOOD"
+                    }
                 ],
-            '_maybe': [
-                {
-                    'display': '_maybe',
-                    'name': 'GOOD',
-                },
-                {
-                    'display': '_alone',
-                    'name': 'BAD'
-                }
-            ]
+            "_maybe": //third basket name (to localize) can add more baskets
+                [
+                    {
+                        "display": "_maybe",
+                        "name": "GOOD"
+                    }
+                ]
         }
     }
 };
